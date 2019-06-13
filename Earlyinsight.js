b648a780eebf0ecbd9e904f9760d14ce4663c714
Celina Dong <celinady25@gmail.com>
2019-06-13 11:14:59 +0100

    Changed the whole css and the website layout

--- Earlyinsight.js
+++ Earlyinsight.js
@@ -48,6 +48,14 @@ var scenes =
     ];
 
 
+var input = document.getElementById('submit');
+input.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("searchButton").click();
+  }
+});
+
 
 function searchScenes() {
   
@@ -80,65 +88,59 @@ function searchScenes() {
 
   for (i = 0; i < finalScenes.length; i++) {
     
-    document.write("<figure><img src=" + finalScenes[i].screenshot + " width= 30% height= 30% style=padding:5px;><figcaption>"+finalScenes[i].description+"</figcaption></figure>");
-    
-    /*var desc = finalScenes[i].description;
-    document.write(desc.bold());
-
-    //document.write(finalScenes[i].description);
-
-    
-    text = finalScenes[i].description;
-    getDiv = document.getElementById('scDescriptions');
-    */
+    //document.write("<figure><img class='sceneImage' src=" + finalScenes[i].screenshot + "><figcaption class='sceneDescription'>"+finalScenes[i].description+"</figcaption></figure>");
+    //document.write("<img class=sceneImage src=" + finalScenes[i].screenshot + "><span class=sceneDescription>"+finalScenes[i].description+"</span>");
+    document.write("<img class='sceneImage' src=" + finalScenes[i].screenshot + ">");
+    document.writeln("<span class='sceneDescription'>" + finalScenes[i].description + "</span>");
     
   }
  
 }
     
-  <img src="../PrimeVideo/Screenshots/scene1.png" onclick="loadVideo1()" value="Load Video 1" />
-      
-  function loadVideo1() {
+
+/*      
+function loadVideo1() {
  var videoEl = document.getElementsByTagName('video')[0];
  var sourceEl = videoEl.getElementsByTagName('source')[0];
  sourceEl.src = 'AmazonEarlyInsights.mp4';
  videoEl.load();
 }
- <img src="../PrimeVideo/Screenshots/scene2.png" onclick="loadVideo2()" value="Load Video 2" />
+
       
-  function loadVideo2() {
+function loadVideo2() {
  var videoE2 = document.getElementsByTagName('video')[0];
  var sourceE2 = videoE2.getElementsByTagName('source')[0];
  sourceE2.src = 'AmazonEarlyInsights.mp4';
  videoE2.load();
 }
 
-  <img src="../PrimeVideo/Screenshots/scene3.png" onclick="loadVideo3()" value="Load Video 3" />
+
       
-  function loadVideo3() {
+function loadVideo3() {
  var videoE3 = document.getElementsByTagName('video')[0];
  var sourceE3 = videoE3.getElementsByTagName('source')[0];
  sourceE3.src = 'AmazonEarlyInsights.mp4';
  videoE3.load();
-  }
+}
       
-  <img src="../PrimeVideo/Screenshots/scene4.png" onclick="loadVideo4()" value="Load Video 4" />
+
       
-  function loadVideo4() {
+function loadVideo4() {
  var videoE4 = document.getElementsByTagName('video')[0];
  var sourceE4 = videoE4.getElementsByTagName('source')[0];
  sourceE4.src = 'AmazonEarlyInsights.mp4';
  videoE4.load();
-  }
+}
       
-  <img src="../PrimeVideo/Screenshots/scene5.png" onclick="loadVideo5()" value="Load Video 5" />
+
       
-  function loadVideo5() {
+function loadVideo5() {
  var videoE5 = document.getElementsByTagName('video')[0];
  var sourceE5 = videoE5.getElementsByTagName('source')[0];
  sourceE5.src = 'AmazonEarlyInsights.mp4';
  videoE5.load();
 }
+*/
 
 
 
